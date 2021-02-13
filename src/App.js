fd334204fbdc6accdf24f3f77366cfdfe4c1f76d
Issouf Zabre <sheridanzabre@gmail.com>
2021-02-12 20:04:59 -0500

    add cool backgroond-particles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component  } from "react";
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinForm from './components/ImageLinForm/ImageLinForm';
+import Rank from './components/Rank/Rank';
+
 // import FaceRecognitionfrom './components/FaceRecognition/FaceRecognition';
 
 
@@ -16,8 +19,23 @@ class App extends Component {
   render(){
     return (
       <div className="App">
+
+      <Particles className="particles"
+        params={{
+          particles: {
+            line_linked: {
+              shadow: {
+            	  enable: true,
+            		color: "#3CA9D1",
+            		blur: 5
+            	}
+            }
+          }
+        }}
+      />
         <Navigation />
         <Logo />
+        < Rank />
         <ImageLinForm />
         {/* <FaceRecognition /> */}
       </div>
